24af4e1f2d60e3c26237acf325aa2d2221ec293e
Joshua <jwaiswa7@gmail.com>
2021-12-04 14:58:28 -0600

    Set devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+       devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:phone_number, :first_name, :other_names, :email, :password)}
+       devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :current_password)}
+  end
+
 end
