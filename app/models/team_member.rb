a1f6830dd91d0cb26bf374ec5c831d4e880fef2e
Joshua <jwaiswa7@gmail.com>
2021-12-12 17:05:47 -0600

    Add role to team member

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -1,4 +1,17 @@
 class TeamMember < ApplicationRecord
   belongs_to :user
   belongs_to :team
+
+  validates :team, :user, :role, presence: true
+
+  before_validation :set_default_role, on: :create
+
+  validates :role, :inclusion => { :in => %w[admin user] }
+
+
+  private
+
+  def set_default_role
+    self.role = "admin"
+  end
 end
